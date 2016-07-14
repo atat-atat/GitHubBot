3b24822216ef3b718888393436a25c0b7d7624be
AtAt <oppy.guseru@gmail.com>
2016-07-14 15:43:28 -0700

    added support for adding your own authentication, and handled rate limiting a little bit

--- cogs/github.py
+++ cogs/github.py
@@ -34,7 +34,7 @@ class GitHubCog:
 		"""
 		Notifies users when a repository has a new commit.
 		"""
-		async with aiohttp.get('https://api.github.com/repos/{}/{}/commits?per_page=1'.format(repo_owner, repo_name)) as r:
+		async with aiohttp.get('https://api.github.com/repos/{}/{}/commits?per_page=1?client_id={}?client_secret={}?{}'.format(repo_owner, repo_name, settings['github']['client_id'], settings['github']['client_secret'], settings['github']['access_token'])) as r:
 			response = await r.json()
 
 		if 'message' in response:
@@ -67,7 +67,7 @@ class GitHubCog:
 
 		await self.bot.say("You will now receive notifications when a new commit has been pushed from this repository, or if an issue was filed.")
 
-	@github.command(name='del_repo', pass_context=True)
+	@github.command(name='del_repo', pass_context=True, no_pm=True)
 	async def github_remove_repo(self, ctx, repo_owner : str, repo_name : str):
 		"""
 		Disables notifications from a repository.
@@ -90,52 +90,66 @@ class GitHubCog:
 						for repo_owner in git_json["repositories"][server][channel]:
 							for repo in git_json["repositories"][server][channel][repo_owner]:
 
-								url = "https://api.github.com/repos/{}/{}/commits?per_page=1".format(repo_owner, repo)
+								url = "https://api.github.com/repos/{}/{}/commits?per_page=1?client_id={}?client_secret={}?access_token={}".format(repo_owner, repo, settings['github']['client_id'], settings['github']['client_secret'], settings['github']['access_token'])
+								print(url)
 
 								async with aiohttp.get(url) as r:
 									response = await r.json()
 
-								latest_commit = response[0]["commit"]["message"]
-								commit_url = response[0]["html_url"]
-								if len(latest_commit) > 30:
-									latest_commit = latest_commit[:50] + "..."
-								date = response[0]["commit"]["author"]["date"]
-								committer = response[0]["commit"]["author"]["name"]
-								fmt = "{} --\n{}\n**Pushed by {} ({})**".format(commit_url, latest_commit, committer, date)
+								if 'message' in response:
+									await self.bot.send_message(discord.Object(channel), "MESSAGE RESP: " + response["message"])
+								else:
 
-								if repo_owner not in self.repo_data:
-									self.repo_data[repo_owner] = response[0]["sha"]
-									await self.bot.send_message(discord.Object(channel), fmt)
-								elif response[0]["sha"] != self.repo_data[repo_owner]:
-									self.repo_data[repo_owner] = response[0]["sha"]
-									await self.bot.send_message(discord.Object(channel), fmt)
+									latest_commit = response[0]["commit"]["message"]
+									commit_url = response[0]["html_url"]
+									if len(latest_commit) > 30:
+										latest_commit = latest_commit[:50] + "..."
+									date = response[0]["commit"]["author"]["date"]
+									committer = response[0]["commit"]["author"]["name"]
+									fmt = "{} --\n{}\n**Pushed by {} ({})**".format(commit_url, latest_commit, committer, date)
 
-								issues_url = "https://api.github.com/repos/{}/{}/issues?per_page=1".format(repo_owner, repo)
-
-								async with aiohttp.get(issues_url) as r:
-									response = await r.json()
-
-								if len(response) > 0:
-									issue_id = response[0]["id"]
-									issue_num = response[0]["number"]
-									issue_title = response[0]["title"]
-									issue_submitter = response[0]["user"]["login"]
-									issue_url = response[0]["html_url"]
-									issue_date = response[0]["created_at"]
-
-									fmt = "{} -- *Issue {}*\n{}\n**Issued by {} ({})**".format(issue_url, issue_num, issue_title, issue_submitter, issue_date)
-
-									if repo not in self.repo_issues:
-										self.repo_issues[repo] = issue_id
+									if channel not in self.repo_data:
+										self.repo_data[channel] = {}
+										self.repo_data[channel][repo] = response[0]["sha"]
 										await self.bot.send_message(discord.Object(channel), fmt)
-									elif self.repo_issues[repo] != issue_id:
-										self.repo_issues[repo] = issue_id
+									elif repo not in self.repo_data[channel]:
+										self.repo_data[channel][repo] = response[0]["sha"]
 										await self.bot.send_message(discord.Object(channel), fmt)
+									elif self.repo_data[channel][repo] != response[0]["sha"]:
+										self.repo_data[channel][repo] = response[0]["sha"]
+										await self.bot.send_message(discord.Object(channel), fmt)
+
+									issues_url = "https://api.github.com/repos/{}/{}/issues?per_page=1?client_id={}?client_secret={}?access_token={}".format(repo_owner, repo, settings['github']['client_id'], settings['github']['client_secret'], settings['github']['access_token'])
+									print(issues_url)
+
+									async with aiohttp.get(issues_url) as r:
+										response = await r.json()
+
+									if len(response) > 0:
+										issue_id = response[0]["id"]
+										issue_num = response[0]["number"]
+										issue_title = response[0]["title"]
+										issue_submitter = response[0]["user"]["login"]
+										issue_url = response[0]["html_url"]
+										issue_date = response[0]["created_at"]
+
+										fmt = "{} -- *Issue {}*\n{}\n**Issued by {} ({})**".format(issue_url, issue_num, issue_title, issue_submitter, issue_date)
+
+										if channel not in self.repo_issues:
+											self.repo_issues[channel] = {}
+											self.repo_issues[channel][repo] = issue_id
+											await self.bot.send_message(discord.Object(channel), fmt)
+										elif repo not in self.repo_issues[channel]:
+											self.repo_issues[channel][repo] = issue_id
+											await self.bot.send_message(discord.Object(channel), fmt)
+										elif self.repo_issues[channel][repo] != issue_id:
+											self.repo_issues[channel][repo] = issue_id
+											await self.bot.send_message(discord.Object(channel), fmt)
 
 			except Exception as e:
 				print("LOOP_ERROR@git_loop! " + self.return_traceback(*sys.exc_info()))
 			
-			await asyncio.sleep(50)
+			await asyncio.sleep(4000)
 
 	def return_traceback(self, etype, value, tb, limit=None, file=None, chain=True):
 		if file is None:
